6476f624d6a9962c97f919993cb5ef1415519de1
Ivo <just@ivo.fyi>
2023-02-02 21:41:30 +0000

    Correção ao commit anterior, faltaram ficheiros

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Highlights from './components/Highlights';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Highlights />
     </div>
   );
 }
