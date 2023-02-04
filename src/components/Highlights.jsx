d81f20941baac4fabb95dfb84e4dc88b8c00885b
Ivo <just@ivo.fyi>
2023-02-04 10:19:46 +0000

    Implementado um API dummy para os destaques a substituir o array estático anterior

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,27 +1,16 @@
+import { useEffect, useState } from "react";
+
 export default function Highlights() {
 
-    const highlights = [
-        {
-            image: "https://cache.dominos.com/wam/prod/market/PT/_pt/images/promo/1e650d66-b3f6-4210-9d3a-bc2813d2f1d2.png",
-            url: "https://www.dominospizza.pt/pt/pages/order/?couponCode_0=PT000155&partnerCode=DOMINOS&so=hp&panelname=&panelnumber=",
-            description: "50% DESCONTO PIZZA 2ª A 5º"
-        },
-        {
-            image: "https://cache.dominos.com/wam/prod/market/PT/_pt/images/promo/f970e8e5-1cf1-4865-901f-3a09c1c12c51.png",
-            url: "https://www.dominospizza.pt/pt/pages/order/?couponCode_0=PT000276&partnerCode=DOMINOS&so=hp&panelname=&panelnumber=",
-            description: "Pizza Média + Lata MANDA VIR | 9,95€"
-        },
-        {
-            image: "https://cache.dominos.com/wam/prod/market/PT/_pt/images/promo/10bceb3f-ed60-4de7-8e97-24c04183fcaf.png",
-            url: "https://www.dominospizza.pt/pt/pages/order/?couponCode_0=PT000348&partnerCode=DOMINOS&so=hp&panelname=&panelnumber=",
-            description: "1 Pizza A Maior + 1 Acompanhamento ou Sobremesa | 17,95€"
-        },
-        {
-            image: "https://cache.dominos.com/wam/prod/market/PT/_pt/images/promo/cc4fa4e4-9942-44bc-a53f-db35fde21b78.png",
-            url: "https://www.dominospizza.pt/pt/pages/order/?couponCode_0=PT000349&partnerCode=DOMINOS&so=hp&panelname=&panelnumber=",
-            description: "Menu FINAL FOUR 21,95€"
-        }
-    ];
+    const [highlights, setHighlights] = useState([]);
+
+    useEffect(() => {
+
+        fetch("/api/highlights.json")
+        .then(response => response.json())
+        .then(data => setHighlights(data));
+
+    }, []);
 
     return (
         <main id="highlights">
